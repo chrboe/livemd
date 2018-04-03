6ab1b7b5a6c128fb88036fac47fcec9406451637
Christoph Böhmwalder <christoph@boehmwalder.at>
2018-04-03 19:08:40 +0200

    cleanup

--- livemd.go
+++ livemd.go
@@ -8,30 +8,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	_ "github.com/chrboe/livemd/statik"
 	"github.com/fsnotify/fsnotify"
 	"github.com/gorilla/websocket"
+	"github.com/rakyll/statik/fs"
+	"github.com/skratchdot/open-golang/open"
 	"gopkg.in/russross/blackfriday.v2"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 	"text/template"
-	"github.com/rakyll/statik/fs"
-	_ "github.com/chrboe/livemd/statik"
-	"flag"
-	"github.com/skratchdot/open-golang/open"
-	"net"
 )
 
 var (
 	messageBuf htmlMessage
-	target     string
 	sockets    []*websocket.Conn
 	titleRegex *regexp.Regexp = regexp.MustCompile(`(?s)^\s*<h1>(.*)</h1>.*$`)
-	pwd string
+	pwd        string
+	upgrader   = websocket.Upgrader{}
+)
+
+const (
+	templatePath = "/view.html"
 )
 
 type htmlMessage struct {
@@ -54,7 +58,7 @@ func renderMarkdown(markdown []byte) []byte {
 	return blackfriday.Run(markdown, blackfriday.WithNoExtensions())
 }
 
-func updateBuffer() {
+func updateBuffer(target string) {
 	mdBuffer, err := ioutil.ReadFile(target)
 
 	if err != nil {
@@ -72,7 +76,7 @@ func updateBuffer() {
 	}
 }
 
-func setupWatch() (*fsnotify.Watcher, error) {
+func setupWatch(target string) (*fsnotify.Watcher, error) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return watcher, err
@@ -89,7 +93,7 @@ func setupWatch() (*fsnotify.Watcher, error) {
 					}
 
 					if abs == target {
-						updateBuffer()
+						updateBuffer(target)
 					}
 				}
 			case err := <-watcher.Errors:
@@ -98,10 +102,13 @@ func setupWatch() (*fsnotify.Watcher, error) {
 		}
 	}()
 
+	err = watcher.Add(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return watcher, nil
 }
-
-var upgrader = websocket.Upgrader{}
 
 func registerUpdate(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -114,43 +121,85 @@ func registerUpdate(w http.ResponseWriter, r *http.Request) {
 	c.WriteJSON(messageBuf)
 }
 
-func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "livemd\n\n")
-		fmt.Fprintf(os.Stderr, "Usage:\n")
-		fmt.Fprintf(os.Stderr, "\t%s [-b] <file>\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "\nOptions:\n")
-		fmt.Fprintf(os.Stderr, "\t-b\tOpen a browser window with the markdown document\n")
-		fmt.Fprintf(os.Stderr, "\t-p\tWhich port to start the webserver on (default: 8081)\n")
+func loadTemplate(path string) (*template.Template, error) {
+	// load statikFS
+	sfs, err := fs.New()
+	if err != nil {
+		return nil, err
 	}
 
+	tmplFile, err := sfs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	tmplBin, err := ioutil.ReadAll(tmplFile)
+	if err != nil {
+		return nil, err
+	}
+
+	tmpl, err := template.New("view").Parse(string(tmplBin))
+	if err != nil {
+		return nil, err
+	}
+
+	return tmpl, nil
+}
+
+func handleHttpRequest(w http.ResponseWriter, r *http.Request, tmpl *template.Template) {
+	data := struct {
+		Title    string
+		Rendered string
+		WsUrl    string
+	}{
+		messageBuf.Title,
+		string(messageBuf.Html),
+		"ws://" + r.Host + "/update",
+	}
+
+	tmpl.Execute(w, data)
+}
+
+// print usage information
+func usage() {
+	fmt.Fprintf(os.Stderr, "livemd\n\n")
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "\t%s [-b] <file>\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	fmt.Fprintf(os.Stderr, "\t-b\tOpen a browser window with the markdown document\n")
+	fmt.Fprintf(os.Stderr, "\t-p\tWhich port to start the webserver on (default: 8081)\n")
+}
+
+func main() {
+	flag.Usage = usage
+
+	// define flags
 	openBrowser := flag.Bool("b", false, "")
 	port := flag.Int("p", 8081, "")
 
+	// parse flags
 	flag.Parse()
 
+	// check if there is exactly one trailing argument (filename)
 	if len(flag.Args()) != 1 {
 		flag.Usage()
 		return
 	}
 
-	var err error
-
-	statikFS, err := fs.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// get target filename
 	relTarget := flag.Args()[0]
-	target, err = filepath.Abs(relTarget)
+
+	target, err := filepath.Abs(relTarget)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	updateBuffer()
+	// convert the document first
+	updateBuffer(target)
 
-	watcher, err := setupWatch()
+	// setup the file watch
+	watcher, err := setupWatch(target)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -159,41 +208,21 @@ func main() {
 
 	log.Print("Watching \"", relTarget, "\" for changes")
 
-	err = watcher.Add(".")
+	// load the template
+	tmpl, err := loadTemplate(templatePath)
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	tmplFile, err := statikFS.Open("/view.html")
-	if err != nil {
-		panic(err)
-	}
-
-	tmplBin, err := ioutil.ReadAll(tmplFile)
-	if err != nil {
-		panic(err)
-	}
-
-	tmpl, err := template.New("view").Parse(string(tmplBin))
-	if err != nil {
-		panic(err)
-	}
-
+	// register http handlers
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := struct {
-			Title    string
-			Rendered string
-			WsUrl    string
-		}{
-			messageBuf.Title,
-			string(messageBuf.Html),
-			"ws://" + r.Host + "/update",
-		}
-		tmpl.Execute(w, data)
+		handleHttpRequest(w, r, tmpl)
 	})
 
 	http.HandleFunc("/update", registerUpdate)
 
+	// listen (, open browser,) and serve
 	log.Printf("Serving on port %d\n", *port)
 
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
